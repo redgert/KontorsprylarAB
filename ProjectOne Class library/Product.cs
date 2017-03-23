4222b791cc27e00d0f305002aa97849ab9d1569d
redgert <niklas@redgert.com>
2017-03-23 13:51:20 +0100

    Add project files.

---
+++ ProjectOne Class library/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne_Class_library
+{
+    class Product
+    {
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+
+        public Product(int productID, int price, int stock, string shortDescription, string longDescription)
+        {
+            ProductID = productID;
+            Price = price;
+            Stock = stock;
+            ShortDescription = shortDescription;
+            LongDescription = longDescription;
+        }
+
+    }
+}
---
+++ ProjectOne Class library/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne_Class_library
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public User(int userID, string username, string password, string firstname, string lastname, string street, string zip, string city, string country, string phonenumber, string email)
+        {
+            UserID = userID;
+            Username = username;
+            Password = password;
+            FirstName = firstname;
+            LastName = lastname;
+            Street = street;
+            Zip = zip;
+            City = city;
+            Country = country;
+            PhoneNumber = phonenumber;
+            Email = email;
+        }
+        public User(int userID, string username, string password, string firstname, string lastname, string street, string zip, string city, string country, string email)
+        {
+            UserID = userID;
+            Username = username;
+            Password = password;
+            FirstName = firstname;
+            LastName = lastname;
+            Street = street;
+            Zip = zip;
+            City = city;
+            Country = country;
+            PhoneNumber = null;
+            Email = email;
+        }
+    }
+}
