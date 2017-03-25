3074abef3a9dbd16cb373c799adb481c1e0022bd
Administrator <Administrator@ACADEMY-6303PF1>
2017-03-25 12:51:26 +0100

    2017-03-25 Alex: Added a Order class

---
+++ ProjectOne Class library/Orders.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOne_Class_library
+{
+    class Order
+    {
+     
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime OrderDate { get; set; }
+
+
+        //TODO You need to update the database SOFIA!!!
+
+        //TODO Do I need to catch up @OutPutID from CreateUser procedure?
+
+        public Order(int OrderID, int UserID, string OrderStatus, DateTime OrderDate) //Constructor for Order 
+        {
+            this.OrderID = OrderID;
+            this.UserID = UserID;
+            this.OrderStatus = OrderStatus;
+            this.OrderDate = OrderDate;
+        }
+
+        public Order()
+
+        {
+
+
+        }
+
+        public override string ToString()
+        {
+            return $" OrderID: {OrderID} UserID: {UserID} OrderStatus: {OrderStatus} OrderDate: {OrderDate}";
+
+        }
+
+    }
+}
---
+++ TestBench/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectOne_Class_library;
+
+namespace TestBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQL.GetAllOrder(2);
+        }
+    }
+}
