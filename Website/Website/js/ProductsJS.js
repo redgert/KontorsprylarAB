91b0aee53e9b73d0ee394eec8471a5fb29285916
ZodiacGemini <alexanderedvinsson@icloud.com>
2017-03-26 11:13:48 +0200

    Added javascript to productpage, also tried getting and appending the products we have, WITH SUCCESS!!

---
+++ Website/Website/js/ProductsJS.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    $.getJSON("/svc/productInfo.aspx").done(function (product) {
+        console.log(product);
+        product.forEach(function (data) {
+            var $myRow = $('<div/>', { class: 'row' })
+            $('#Products').append($myRow)
+            console.log(data);
+
+            $myRow.append($('<p/>', {
+                class: 'productcontainer',
+                text: data.ShortDescription
+            }));
+        });
+    });
+});
+
