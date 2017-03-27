6400479e646529a8b8f7e27af77c368f54217801
Administrator <Administrator@ACADEMY-6303P7Q>
2017-03-27 21:57:26 +0200

    Tried to fix the log out button, not working yet...

---
+++ Website/Website/svc/logOutUser.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website.svc
+{
+    public partial class logOutUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Remove this when the Logout-method is changed from JSON to something else...
+            logOutUserLiteral.Text = "temporary, unecessary text";
+
+            Session["user"] = null;
+        }
+    }
+}
