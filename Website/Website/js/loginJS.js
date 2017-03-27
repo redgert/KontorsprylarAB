270330fe8061475e21887c100570c27826663118
Administrator <Administrator@ACADEMY-6303P7Q>
2017-03-27 16:10:11 +0200

    Added Drop-down menu to signed in user

--- Website/Website/js/loginJS.js
+++ Website/Website/js/loginJS.js
@@ -7,14 +7,14 @@ function GetRequest() {
             if (user.IsAdmin === 0) {
                 window.location.href = "/home.aspx";
                 //NEVER MIND THIS BELOW RIGHT NOW
-                
+
             }
             else if (user.IsAdmin === 1) {
                 window.location.href = "/home.aspx";
                 //NEVER MIND THIS BELOW RIGHT NOW
                 $('#admin').removeProp($('display'));
             }
-            //If user does not exist, or username and password doesnt match, the JSON object is set to "Error"
+                //If user does not exist, or username and password doesnt match, the JSON object is set to "Error"
             else if (user === "Error") {
                 console.log(user);
                 alert("Wrong username or password");
@@ -26,10 +26,15 @@ $(document).ready(function () {
     $.getJSON("svc/info.aspx?loggedin=1").done(function (user) {
         console.log(user);
         $('#loginButton').remove();
+        $('#listSignUp').remove();
+        $('#listLoginButton').addClass("dropdown");
         $('#listLoginButton').append($('<a/>', {
-                    href: '#',
-                    text: user.FirstName + " " + user.LastName,
-        }));
+            href: '#',
+            text: user.FirstName + " " + user.LastName,
+            class: 'dropdown-toggle',
+            'data-toggle': "dropdown",
+        })).append($('<ul class="dropdown-menu"><li><a href="#">Page 1-1</a></li><li><a href="#">Page 1-2</a></li><li><a href="#">Page 1-3</a></li></ul>'));
+        
         if (user.IsAdmin === 1) {
             $('#adminList').append($('<a/>', {
                 text: 'Admin',
