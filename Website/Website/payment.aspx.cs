6ba30e477af412cffe215e61232e02e17cb833f4
ZodiacGemini <alexanderedvinsson@icloud.com>
2017-03-28 19:46:54 +0200

    BUY NOW!!!!!

---
+++ Website/Website/payment.aspx.cs
@@ -0,0 +1,44 @@
+﻿using ProjectOne_Class_library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website
+{
+    public partial class payment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                double totCost = 0;
+                List<Product> ourProdList = new List<Product>();
+                List<ProductList> prodList = new List<ProductList>();
+                SQL mySQL = new SQL();
+                User myUser = mySQL.GetUser(Session["user"].ToString());
+
+                var myList = SQL.GetOrder(myUser.UserID);
+
+                foreach (var item in myList)
+                {
+                    prodList = SQL.GetProductList(item.OrderID);
+                }
+
+                foreach (var product in prodList)
+                {
+                    ourProdList.Add(mySQL.GetProduct(product.ProductID));
+                }
+
+                foreach (var price in ourProdList)
+                {
+                    totCost += Convert.ToDouble(price.Price);
+                }
+
+                LabelPrice.Text = totCost.ToString();
+            }
+        }
+    }
+}
